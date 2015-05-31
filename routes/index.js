8db803c8786b732d5db720a9abb4fd4eb6447918
jcantosm <jcantosmoreno@gmail.com>
2015-05-31 11:16:20 +0000

    Paso 3: Primera pregunta.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+/* GET de pregunta */
+router.get('/quizes/question', quizController.question);
+
+/* GET de respuesta */
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
