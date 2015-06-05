13d4322208de0f68d1be2a7affc77212df88d1c5
jcantosm <jcantosmoreno@gmail.com>
2015-06-05 19:54:39 +0000

    Paso 7: Base de datos: sequelize.js y SQLite.

---
+++ models/quiz.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define(
+        'Quiz', {
+            pregunta: {
+                type: DataTypes.STRING
+            },
+            respuesta: {
+                type: DataTypes.STRING
+            }
+        }
+    );
+}
