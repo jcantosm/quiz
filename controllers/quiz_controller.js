d8c1e4124af4d1ad8a9a26e5fe887bccbee2c46e
jcantosm <jcantosmoreno@gmail.com>
2015-06-07 11:12:26 +0000

    Paso 12: Validación de entradas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,7 +17,8 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res) {
     models.Quiz.findAll().then(function(quizes) {
         res.render('quizes/index', {
-            quizes: quizes
+            quizes: quizes,
+            errors: []
         });
     });
 };
@@ -25,21 +26,44 @@ exports.index = function(req, res) {
 // GET /quizes/:id
 exports.show = function(req, res) {
     res.render('quizes/show', {
-        quiz: req.quiz
+        quiz: req.quiz,
+        errors: []
     });
 };
 
 // GET /quizes/new
 exports.new = function(req, res) {
-    var quiz = models.Quiz.build({'pregunta': '', 'respuesta': ''});
-    res.render('quizes/new', {quiz: quiz});
+    var quiz = models.Quiz.build({
+        'pregunta': '',
+        'respuesta': ''
+    });
+    res.render('quizes/new', {
+        quiz: quiz,
+        errors: []
+    });
 };
 
 // GET /quizes/create
-exports.create  = function(req, res) {
+exports.create = function(req, res) {
     var quiz = models.Quiz.build(req.body.quiz);
-    quiz.save({fields: ['pregunta', 'respuesta']}).then(function() {
-        res.redirect('/quizes');
+
+    console.log('PREGUNTA  := \'' + quiz.pregunta + '\'\n');
+    console.log('RESPUESTA := \'' + quiz.respuesta + '\'\n');
+    
+    quiz.validate().then(function(error) {
+        if (error) {
+            res.render('quizes/new', {
+                quiz: quiz,
+                errors: error.errors
+            });
+        }
+        else {
+            quiz.save({
+                fields: ['pregunta', 'respuesta']
+            }).then(function() {
+                res.redirect('/quizes');
+            });
+        }
     });
 };
 
@@ -54,7 +78,8 @@ exports.answer = function(req, res) {
     }
     res.render('quizes/answer', {
         quiz: req.quiz,
-        respuesta: resultado
+        respuesta: resultado,
+        errors: []
     });
 
 };
--- models/quiz.js
+++ models/quiz.js
@@ -2,10 +2,12 @@ module.exports = function(sequelize, DataTypes) {
     return sequelize.define(
         'Quiz', {
             pregunta: {
-                type: DataTypes.STRING
+                type: DataTypes.STRING,
+                validate: { notEmpty: {msg: '-> Falta Pregunta'} }
             },
             respuesta: {
-                type: DataTypes.STRING
+                type: DataTypes.STRING,
+                validate: { notEmpty: {msg: '-> Falta Respuesta'} }
             }
         }
     );
